9b7f08b52bffd893e9fc1959225386689804ee1f
Col-R <2colewestin@gmail.com>
2022-01-24 18:56:18 -0600

    first commit

---
+++ src/main/java/com/cole/snapline/SnaplineApplication.java
@@ -0,0 +1,13 @@
+package com.cole.snapline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaplineApplication.class, args);
+	}
+
+}
