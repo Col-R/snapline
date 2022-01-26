11d80611d7cb7ba6ac0753c9e18c23ebd8e25865
Col-R <2colewestin@gmail.com>
2022-01-25 20:44:35 -0600

    imported javaxpersistence for model

--- src/main/java/com/dojo/snapline/models/User.java
+++ src/main/java/com/dojo/snapline/models/User.java
@@ -4,11 +4,13 @@ import java.beans.Transient;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
