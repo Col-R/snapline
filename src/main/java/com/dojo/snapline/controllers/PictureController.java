a5299b3c0d064a49d754b85112f0a2b49401647b
Col-R <2colewestin@gmail.com>
2022-01-26 07:08:02 -0600

    started on dashboard controller

--- src/main/java/com/dojo/snapline/controllers/PictureController.java
+++ src/main/java/com/dojo/snapline/controllers/PictureController.java
@@ -1,5 +1,27 @@
 package com.dojo.snapline.controllers;
 
-public class PictureController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import com.dojo.snapline.services.UserService;
+
+public class PictureController {
+	@Autowired
+	private PhotoService pService;
+	@Autowired
+	private UserService uService;
+	@Autowired
+	private UserValidator validator
+
+	@GetMapping("/dashboard")
+	public String index(Model viewModel, HttpSession session) {
+		if(session.getAttribute("user__id") == null) {
+			return "redirect:/";
+		}
+		viewModel.addAttribute("user", this.uService.getOneUser((Long)session.getAttribute("user__id")));
+		viewModel.addAttribute("allAlbums", this.pService.getAllAlbums());
+		return "index.jsp";
+	}
 }
