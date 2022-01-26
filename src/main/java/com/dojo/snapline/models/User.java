84854b7e40b3a0a2dae5141d7de632dbdcf52539
Col-R <2colewestin@gmail.com>
2022-01-25 20:39:10 -0600

    created table for user model

--- src/main/java/com/dojo/snapline/models/User.java
+++ src/main/java/com/dojo/snapline/models/User.java
@@ -1,5 +1,38 @@
 package com.dojo.snapline.models;
 
-public class User {
+import java.beans.Transient;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Size(max=20)
+	private String firstName;
+	@NotBlank
+	@Size(max=30)
+	private String lastName;
+	@Email  //regex
+	private String email;
+	private String username;
+	private String password;
+	@Transient
+	private String confirmPassword;
+	
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List <Picture> myPictures;
 }
