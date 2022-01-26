fe4c636d7cfb4fc0c64ac9e851efd09a73e7a4fc
Col-R <2colewestin@gmail.com>
2022-01-26 16:13:35 -0600

    starter queries for picture repository

--- src/main/java/com/dojo/snapline/repositories/PictureRepository.java
+++ src/main/java/com/dojo/snapline/repositories/PictureRepository.java
@@ -1,5 +1,13 @@
 package com.dojo.snapline.repositories;
 
-public interface PictureRepository {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import com.dojo.snapline.models.Picture;
+
+@Repository
+public interface PictureRepository {
+	List<Picture> findAll();
+	List<Picture> findAllOrderByCreated_AtDesc();
 }
