b4526639d4ef324271d9537840ab4d976fa92aa0
Col-R <2colewestin@gmail.com>
2022-01-26 16:37:28 -0600

    added to repository, and removed from picture controller for now

--- src/main/java/com/dojo/snapline/repositories/PictureRepository.java
+++ src/main/java/com/dojo/snapline/repositories/PictureRepository.java
@@ -2,12 +2,18 @@ package com.dojo.snapline.repositories;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dojo.snapline.models.Picture;
+import com.dojo.snapline.models.User;
 
 @Repository
 public interface PictureRepository extends CrudRepository <Picture, Long>{
+	// get all pictures
 	List<Picture> findAll();
-	List<Picture> findAllOrderByCreated_AtDesc();
+	// all pictures ordered chronologically
+	List<Picture> findAllOrderBycreatedAtDesc();
+	// all pictures by user
+	List<Picture> findAllByUser (User owner);
 }
