a8fcb50d02fb88a55730f88baf6df627dde7fb4f
Col-R <2colewestin@gmail.com>
2022-01-26 20:47:28 -0600

    added dashboard controller

--- src/main/java/com/dojo/snapline/controllers/LoginController.java
+++ src/main/java/com/dojo/snapline/controllers/LoginController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -35,18 +36,28 @@ public class LoginController {
 		}
 		User newUser = this.uService.registerUser(user);
 		session.setAttribute("user__id", newUser.getId());
-		return "redirect:/"; //this will redirect to whatever our login page is 
+		return "redirect:/dashboard";
 	}
 	@PostMapping("/login")
 	public String login(HttpSession session, @RequestParam("lemail") String email, @RequestParam("lpassword")String password, RedirectAttributes redirectAttr) {
 		if (!this.uService.authenticateUser(email, password)) {
 			redirectAttr.addFlashAttribute("loginError", "Invalid Credentials");
-			return "redirect:/";
+			return "redirect:/dashboard";
 		}
 		User user = this.uService.getUserByEmail(email);
 		session.setAttribute("user__id", user.getId());
 		return "redirect:/";
 	}
+	@GetMapping("dashboard")
+	public String dashboard (Model viewModel, HttpSession session) {
+		if(session.getAttribute("user__id") == null) {
+			return "redirect:/";
+		}
+		viewModel.addAttribute("user", this.uService.getOneUser((Long)session.getAttribute("user__id")));
+		return "dashboard.jsp";
+	}
+	
+	
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
--- src/main/java/com/dojo/snapline/controllers/PictureController.java
+++ src/main/java/com/dojo/snapline/controllers/PictureController.java
@@ -1,11 +1,13 @@
 package com.dojo.snapline.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import com.dojo.snapline.services.PictureService;
 import com.dojo.snapline.services.UserService;
 import com.dojo.snapline.validators.UserValidator;
 
+@Controller
 public class PictureController {
 	@Autowired
 	private PictureService pService;
