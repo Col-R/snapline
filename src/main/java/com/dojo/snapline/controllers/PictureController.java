a8714c99d6cde545aa88a89c9867bf3d808b9298
Col-R <2colewestin@gmail.com>
2022-01-28 18:46:07 -0600

    image upload controller

--- src/main/java/com/dojo/snapline/controllers/PictureController.java
+++ src/main/java/com/dojo/snapline/controllers/PictureController.java
@@ -1,16 +1,25 @@
 package com.dojo.snapline.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.dojo.snapline.models.User;
 import com.dojo.snapline.services.PictureService;
 import com.dojo.snapline.services.UserService;
-import com.dojo.snapline.validators.UserValidator;
 
 @Controller
 @RequestMapping("/home")
@@ -19,8 +28,8 @@ public class PictureController {
 	private PictureService pService;
 	@Autowired
 	private UserService uService;
-	@Autowired
-	private UserValidator validator;
+	
+	private static String UPLOADED_FOLDER = "src/main/resources/static/images";
 
 	@GetMapping("")
 	public String dashboard (Model viewModel, HttpSession session) {
@@ -38,5 +47,24 @@ public class PictureController {
 		}
 		viewModel.addAttribute("user", this.uService.getOneUser((Long)session.getAttribute("user__id")));
 		return "newUpload.jsp";
+	}
+	
+	@PostMapping("/upload") // image upload
+	public String upload(@RequestParam("image") MultipartFile file, @RequestParam("description") String description, HttpSession session, RedirectAttributes redirectAttr ) {
+		User user = uService.getOneUser((Long)session.getAttribute("user__id"));
+		if (file.isEmpty()) {
+			redirectAttr.addFlashAttribute("message", "Upload field cannot be empty");
+			return "redirect:/home/new";
+		}
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+			String url = "/images/" + file.getOriginalFilename();
+			this.pService.uploadPic(user, url, description);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/dashboard";
 	}
 }
